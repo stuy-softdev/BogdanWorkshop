1d27ac07571cb3333eb944df562ba8627c248b04
BogdanSotnikov <bogdans2789@nycstudents.net>
2025-09-22 11:48:17 -0400

    completed weighted list, need to work on accessing value

--- 09_csv-calc/career_chooser.py
+++ 09_csv-calc/career_chooser.py
@@ -3,29 +3,44 @@
 # SoftDev
 # K09 -- csv calc
 # 2025-09-22
-# time spent: 
+# time spent: 0.4
 
 import random
 # Open file, read it
 file = open("occupations.csv", "r")
 content = file.read()
 
+# split the string by newlines into a list, remove extra items (total, empty lines, etc.)
 splitter = content.split("\n")
 splitter = splitter[1:-2]
 
 
-# Split it in the position theyre in, simple
+# turn the list into a dictionary by splitting by last comma
 d = {}
-for i in range (len(splitter)):
-    print(splitter[i])
+weighted_l = []
+for i in range(len(splitter)):
+    #print(splitter[i])
     comma = splitter[i].rindex(',')
-    d[splitter[i][0:comma]] = splitter[i][comma+1:]
+    # create each dictionary key:value with type casting to convert string into a float
+    d[splitter[i][0:comma]] = float(splitter[i][comma+1:])
+    # add key items to weighted list
+    for f in range(int(float(splitter[i][comma+1:])*10)):
+        key1 = splitter[i][0:comma]
+        weighted_l.append([key1])
 
 print(d)
 
-handle, dname = random.choice(list(d.items()))
-print("D1:\n")
-print(f"{handle}:{dname}\n")
+# randomly choose one key value from the weighted dictionary
+handlei = random.randrange(len(weighted_l))
+handle = weighted_l[handlei]
+
+# access value of key item from dictionary
+dname = d.get(str(handle))
+
+print("\nRandom Item:")
+print(f"{handle}: {dname}\n")
+
+
 
 
 
